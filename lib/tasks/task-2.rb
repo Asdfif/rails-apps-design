eefb189df495aa3af887b02c719aa777a02e5e29
Alexandr Burov <a.burov@didlogic.com>
2023-11-01 21:25:28 +0300

    refactor PrepareDelivery

--- lib/tasks/task-2.rb
+++ lib/tasks/task-2.rb
@@ -1,5 +1,13 @@
 class PrepareDelivery
-  TRUCKS = { kamaz: 3000, gazel: 1000 }
+  class DeliveryError < StandardError; end
+  class DeliveryDateError < DeliveryError; end
+  class DeliveryAddressError < DeliveryError; end
+  class DeliveryTruckError < DeliveryError; end
+
+  TRUCKS = { kamaz: 3000, gazel: 1000 }.freeze
+  ERROR_STATUS = "error".freeze
+
+  attr_reader :order, :user
 
   def initialize(order, user)
     @order = order
@@ -7,17 +15,47 @@ class PrepareDelivery
   end
 
   def perform(destination_address, delivery_date)
-    result = { truck: nil, weight: nil, order_number: @order.id, address: destination_address, status: :ok }
-    raise "Дата доставки уже прошла" if delivery_date < Time.current
-    raise "Нет адреса" if destination_address.city.empty? || destination_address.street.empty? || destination_address.house.empty?
+    init_result
+    validate_delivery_date!(delivery_date)
+    validate_destination_address!(destination_address)
+    check_truck!
 
-    weight = @order.products.map(&:weight).sum
-    TRUCKS.keys.each { |key| result[:truck] = key if TRUCKS[key.to_sym] > weight }
-    raise "Нет машины" if result[:truck].nil?
-
+   rescue DeliveryError
+    result[:satus] = ERROR_STATUS
+   ensure
     result
-   rescue StandardError
-     result[:satus] = "error"
+  end
+
+  private
+
+  attr_reader :result
+
+  def init_result
+    @result = { truck: nil, weight: nil, order_number: order.id, address: destination_address, status: :ok }
+  end
+
+  def validate_delivery_date!(delivery_date)
+    raise DeliveryDateError.new("Дата доставки уже прошла") if delivery_date < Time.current
+  end
+
+  def validate_destination_address!(destination_address)
+    raise DeliveryAddressError.new("Нет адреса") if destination_address.city.empty? || destination_address.street.empty? || destination_address.house.empty?
+  end
+
+  def check_truck!
+    raise DeliveryTruckError.new("Нет машин") unless set_available_truck
+  end
+
+  def set_available_truck
+    result[:truck] = available_truck
+  end
+
+  def available_truck
+    TRUCKS.find { |_, v| v > weight }&.first
+  end
+
+  def weight
+    @weight ||= order.products.sum(&:weight)
   end
 end
 
