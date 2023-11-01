8284b4360e3f4da9d33ce14a59dc5ca444ce6388
Alexandr Burov <a.burov@didlogic.com>
2023-11-01 10:40:30 +0300

    after review updates

--- app/models/cloud_payment.rb
+++ app/models/cloud_payment.rb
@@ -1,9 +1,19 @@
 class CloudPayment
-  VALID_STATUS = 'completed'
+  SUCCESS_STATUS = 'completed'
 
   class << self
     def proccess(user_uid:, amount_cents:, currency:)
-      { status: VALID_STATUS }
+      @result = { status: SUCCESS_STATUS }
     end
+
+    def success?
+      return if result.nil?
+
+      result[:status] == SUCCESS_STATUS
+    end
+
+    private
+
+    attr_reader :result
   end
 end
--- app/service/gateway_processor.rb
+++ app/service/gateway_processor.rb
@@ -5,21 +5,22 @@ module Service
     step :check_payment_status
     step :create_product_access
     step :send_order_mail
+    step :setup_delivery
 
     def find_product(ctx, product_id:, **)
       ctx[:product] = Product.find_by_id(product_id)
     end
 
     def gateway_process(ctx, gateway:, current_user:, amount:, currency:, **)
-      ctx[:payment_result] = gateway.proccess(
+      gateway.proccess(
         user_uid: current_user.cloud_payments_uid,
         amount_cents: amount * 100,
         currency: currency
       )
     end
 
-    def check_payment_status(ctx, payment_result:, **)
-      payment_result[:status] == 'completed'
+    def check_payment_status(ctx, gateway:, **)
+      gateway.success?
     end
 
     def create_product_access(ctx, current_user:, product:, **)
